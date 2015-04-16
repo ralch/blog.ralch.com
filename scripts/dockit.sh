d3f3da16a1e92bb9a76ec880a3c89809d7ba99e0
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-04-16 06:00:33 +0100

    change the dockit file to use grape image

--- scripts/dockit.sh
+++ scripts/dockit.sh
@@ -2,19 +2,18 @@
 
 set -x
 
-HOST_DIR=/var/vcap/jobs/ralch-blog
-CONTAINER_DIR=/var/vcap/jobs/ralch-blog
+#HOST_DIR=/var/vcap/jobs/ralch-web
+HOST_DIR=/Volumes/Macintosh-HD/Users/ralch/Projects/ralch-blog/web/public
+CONTAINER_DIR=/var/www
 NGINX_HOST=blog.ralch.com
 NGINX_PORT=1313
-CONTAINER_ENTRYPOINT="./ralch-blog-server -port=$NGINX_PORT"
 
 docker run -v $HOST_DIR:$CONTAINER_DIR \
            -w $CONTAINER_DIR \
            -e VIRTUAL_HOST=$NGINX_HOST \
            -e VIRTUAL_PORT=$NGINX_PORT \
            --expose $NGINX_PORT \
-           --entrypoint="$CONTAINER_ENTRYPOINT" \
            --restart="always" \
-           --name blog \
-           -d busybox 
+           --name web \
+           -d grape -port $NGINX_PORT
 
